0e431149134de56635803deab9e2cd2bbcfbde7c
Hunter Breathat <nexisdev@gmail.com>
2020-11-10 19:13:11 -0500

    Fix failing test

--- lib/ui/notifications/overlay.dart
+++ lib/ui/notifications/overlay.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nexusos_sdk/nexusos_sdk.dart' as system;
-import 'package:provider/provider.dart';
 
 import 'entry.dart';
 
@@ -16,19 +15,18 @@ class NotificationOverlay extends StatefulWidget {
 class _NotificationOverlayState extends State<NotificationOverlay> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<system.NotificationManager>(
-      builder: (context, manager, child) => Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: manager.pending.length,
-              itemBuilder: (_, index) => NotificationEntry(
-                manager.pending[index],
-              ),
+    final manager = system.NotificationManager();
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: manager.pending.length,
+            itemBuilder: (_, index) => NotificationEntry(
+              manager.pending[index],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
